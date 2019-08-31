0c38e3e06a9e4cf3b4d20385e515e7f9b091e71f
blomqma <markus.blomqvist@anders.fi>
2019-08-31 20:31:02 +0300

    Add user detail serializer

--- src/api/views/user_view_set.py
+++ src/api/views/user_view_set.py
@@ -1,9 +1,19 @@
-from rest_framework import viewsets
-from ..serializers import UserSerializer
 from django.contrib.auth import get_user_model
+from rest_framework import permissions, viewsets
+
+from ..serializers import UserDetailSerializer, UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = get_user_model().objects.all()
     search_fields = ["username"]
+    permission_classes = [permissions.AllowAny]
+
+
+    def get_serializer_class(self):
+        if self.action in ["retrieve", "update", "delete"]:
+            return UserDetailSerializer
+        
+        else:
+            return self.serializer_class

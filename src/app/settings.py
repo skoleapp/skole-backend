c8f7602ed158a404b790135b7a926721f5b3b418
blomqma <markus.blomqvist@anders.fi>
2019-09-07 17:08:26 +0300

    Add cors headers

--- src/app/settings.py
+++ src/app/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "imagekit",
+    "corsheaders",
     "core",
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -148,3 +150,9 @@ REST_FRAMEWORK = {
 }
 
 AUTH_USER_MODEL = "core.User"
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+]
+
+APPEND_SLASH = False

a920d7e9993db0a750ec55e95de4bb20f04bc31c
ruohola <eero@ruohola.dev>
2019-10-10 14:27:37 +0300

    Working on getting different data for public and private User

--- src/api/permissions/__init__.py
+++ src/api/permissions/__init__.py
@@ -1,2 +1,1 @@
-from .common import ReadOnly
-from .user import IsAnonymous, IsOwnerOrReadOnly
+
--- src/api/schemas/user_type.py
+++ src/api/schemas/user_type.py
@@ -3,13 +3,35 @@ from django.contrib.auth import get_user_model
 from graphene_django import DjangoObjectType
 
 
-class UserType(DjangoObjectType):
+class UserTypePublic(DjangoObjectType):
     class Meta:
         model = get_user_model()
+        fields = ("id", "username", "title", "bio", "points", "created", "email")
+
+    def resolve_email(self):
+        raise AssertionError()
+
+
+class UserTypePrivate(DjangoObjectType):
+    class Meta:
+        model = get_user_model()
+        fields = ("id", "username", "title", "bio", "points", "created", "email", "language")
 
 
 class Query(object):
-    all_users = graphene.List(UserType)
+    all_users = graphene.List(UserTypePublic)
+    user = graphene.Field(UserTypePublic, user_id=graphene.Int())
+    me = graphene.Field(UserTypePrivate)
 
     def resolve_all_users(self, info, **kwargs):
         return get_user_model().objects.all()
+
+    def resolve_user(self, info, user_id):
+        if info.context.user.id == user_id:
+            res = UserTypePrivate()
+            return self.resolve_me(info)
+        return get_user_model().objects.get(pk=user_id)
+
+    def resolve_me(self, info):
+        return get_user_model().objects.get(pk=info.context.user.id)
+

e5be30ebfbb5cac551a123434ec7428d5b065c56
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-13 10:59:47 +0300

    Exempt csrf token

--- src/app/urls.py
+++ src/app/urls.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
     path(r"admin/", admin.site.urls),
-    path(r"graphql/", GraphQLView.as_view(graphiql=True))
+    path(r"graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
 
 if settings.DEBUG:

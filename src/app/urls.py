cf9144e446616d53b4c730d8623ef88d24024adc
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-10-17 15:27:09 +0300

    Update url imports

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 

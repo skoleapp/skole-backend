ef3218d44687584a8a0af66a117e4d9ecbc87f3f
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-11-10 15:07:21 +0200

    Rename subject list to subjects

--- src/api/schemas/subject.py
+++ src/api/schemas/subject.py
@@ -14,10 +14,10 @@ class SubjectType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    subject_list = graphene.List(SubjectType)
+    subjects = graphene.List(SubjectType)
     subject = graphene.Field(SubjectType, id=graphene.Int())
 
-    def resolve_subject_list(self, info: ResolveInfo) -> List[Subject]:
+    def resolve_subjects(self, info: ResolveInfo) -> List[Subject]:
         return Subject.objects.all()
 
     def resolve_subject(self, info: ResolveInfo, id: int) -> Subject:

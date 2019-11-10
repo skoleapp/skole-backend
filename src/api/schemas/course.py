3c2b93f9ff7e2fedb5cd8333b2ef0d3e4fc62385
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-11-11 01:05:05 +0200

    Update course schema

--- src/api/schemas/course.py
+++ src/api/schemas/course.py
@@ -19,11 +19,14 @@ class CourseType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    course_list = graphene.List(CourseType)
+    courses = graphene.List(CourseType, subject_id=graphene.Int())
     course = graphene.Field(CourseType, id=graphene.Int())
 
-    def resolve_course_list(self, info: ResolveInfo) -> List[Course]:
-        return Course.objects.all()
+    def resolve_courses(self, info: ResolveInfo, subject_id: int = None) -> List[Course]:
+        if subject_id is not None:
+            return Course.objects.filter(subject__pk=subject_id)
+        else:
+            return Course.objects.all()
 
     def resolve_course(self, info: ResolveInfo, id: int) -> Course:
         return Course.objects.get(pk=id)

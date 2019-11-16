fda02ae9e8d69f2e41596dff87bb1b66e4c3fc41
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-11-16 13:40:28 +0200

    Update subject query

--- src/api/schemas/subject.py
+++ src/api/schemas/subject.py
@@ -15,10 +15,6 @@ class SubjectType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     subjects = graphene.List(SubjectType)
-    subject = graphene.Field(SubjectType, id=graphene.Int())
 
     def resolve_subjects(self, info: ResolveInfo) -> List[Subject]:
         return Subject.objects.all()
-
-    def resolve_subject(self, info: ResolveInfo, id: int) -> Subject:
-        return Subject.objects.get(pk=id)

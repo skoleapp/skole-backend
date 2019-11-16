bbb5ccde16b7f00b54cbf4459fe3010c39a96914
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-11-17 00:34:42 +0200

    Update school API

--- src/api/schemas/school.py
+++ src/api/schemas/school.py
@@ -13,21 +13,43 @@ class SchoolType(DjangoObjectType):
 
     class Meta:
         model = School
-        fields = ("id", "school_type", "name", "city", "country", "subjects")
-
-    def resolve_school_type(self, info: ResolveInfo) -> str:
-        return self.get_school_type_display()
-
-    def resolve_subjects(self, info: ResolveInfo) -> str:
-        return self.subjects.all()
+        fields = ("id", "school_type", "name", "city", "country")
 
 
 class Query(graphene.ObjectType):
-    schools = graphene.List(SchoolType)
-    school = graphene.Field(SchoolType, id=graphene.Int(required=True))
+    schools = graphene.List(
+        SchoolType,
+        school_type=graphene.String(),
+        city=graphene.String(),
+        country=graphene.String(),
+        name=graphene.String()
+    )
+    
+    school = graphene.Field(SchoolType, school_id=graphene.Int())
 
-    def resolve_schools(self, info: ResolveInfo) -> List[School]:
-        return School.objects.all()
+    def resolve_schools(
+        self,
+        info: ResolveInfo,
+        school_type: str = None,
+        city: str = None,
+        country: str = None,
+        name: str = None
+    ) -> List[School]:
+        schools =  School.objects.all()
 
-    def resolve_school(self, info: ResolveInfo, id: int) -> School:
-        return School.objects.get(pk=id)
+        if school_type is not None:
+            schools = schools.filter(school_type=school_type.replace(" ", "_").upper()) # Convert to upper snake case.
+        if city is not None:
+            schools = schools.filter(city__iexact=city)
+        if country is not None:
+            schools = schools.filter(country__iexact=country)
+        if name is not None:
+            schools = schools.filter(name__icontains=name)
+        
+        return schools
+
+    def resolve_school(self, info: ResolveInfo, school_id: int = None) -> School:
+        if school_id is not None:
+            return School.objects.get(pk=school_id)
+        else:
+            return None

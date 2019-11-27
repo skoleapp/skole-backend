d47f40d91cf165414961039336a6c9a40bdbb226
ruohola <eero@ruohola.dev>
2019-11-27 20:36:53 +0200

    Resource parts and it's fixture loading works

--- src/app/models/comment_thread.py
+++ src/app/models/comment_thread.py
@@ -1,16 +1,6 @@
 from django.db import models
 
-from .course import Course
-from .resource import Resource
 
-
-class CourseCommentThread(models.Model):
+class CommentThread(models.Model):
     """Models a comment thread posted on a course."""
-
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="course_comment_threads")
-
-
-class ResourceCommentThread(models.Model):
-    """Models a comment thread posted on a resource."""
-
-    resource = models.ForeignKey(Resource, on_delete=models.CASCADE, related_name="resource_comment_threads")
+    foo = models.CharField(max_length=30)

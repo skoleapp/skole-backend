e8743aab865e70232d79b4abb5e954d0ff77411e
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-12-06 01:23:26 +0200

    Add resource type api

---
+++ src/api/schemas/resource_type.py
@@ -0,0 +1,18 @@
+import graphene
+from graphene_django import DjangoObjectType
+from graphql import ResolveInfo
+from app.models import ResourceType
+from typing import List
+
+
+class ResourceTypeObjectType(DjangoObjectType):
+    class Meta:
+        model = ResourceType
+        fields = ("name",)
+
+
+class Query(graphene.ObjectType):
+    resource_types = graphene.List(ResourceTypeObjectType)
+
+    def resolve_resource_types(self, info: ResolveInfo) -> List[ResourceType]:
+        return ResourceType.objects.all()
--- src/api/schemas/schema.py
+++ src/api/schemas/schema.py
@@ -4,6 +4,8 @@ import api.schemas.course
 import api.schemas.school
 import api.schemas.subject
 import api.schemas.user
+import api.schemas.resource
+import api.schemas.resource_type
 
 
 class Query(
@@ -12,6 +14,7 @@ class Query(
     api.schemas.subject.Query,
     api.schemas.user.Query,
     api.schemas.resource.Query,
+    api.schemas.resource_type.Query
 ):
     pass
 

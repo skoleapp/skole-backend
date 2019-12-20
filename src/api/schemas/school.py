99d20787969349a61f936935318d50d3fb6e017f
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-12-20 15:31:23 +0200

    Update school api

--- src/api/schemas/school.py
+++ src/api/schemas/school.py
@@ -16,7 +16,7 @@ class SchoolType(DjangoObjectType):
 
     class Meta:
         model = School
-        fields = ("id", "school_type", "name", "city")
+        fields = ("id", "name", "city")
 
     def resolve_country(self, info: ResolveInfo) -> Country:
         return self.city.country
@@ -29,39 +29,11 @@ class SchoolType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    schools = graphene.List(
-        SchoolType,
-        school_type=graphene.String(),
-        school_name=graphene.String(),
-        school_city=graphene.String(),
-        school_country=graphene.String(),
-    )
-
+    schools = graphene.List(SchoolType)
     school = graphene.Field(SchoolType, school_id=graphene.Int())
 
-    def resolve_schools(
-            self,
-            info: ResolveInfo,
-            school_type: str = None,
-            school_name: str = None,
-            school_city: str = None,
-            school_country: str = None,
-    ) -> List[School]:
-        schools = School.objects.all()
+    def resolve_schools(self, info: ResolveInfo) -> List[School]:
+        return School.objects.all()
 
-        if school_type is not None:
-            schools = schools.filter(school_type__name=school_type)
-        if school_name is not None:
-            schools = schools.filter(name__icontains=school_name)
-        if school_city is not None:
-            schools = schools.filter(city__name__iexact=school_city)
-        if school_country is not None:
-            schools = schools.filter(city__country__name__iexact=school_country)
-
-        return schools
-
-    def resolve_school(self, info: ResolveInfo, school_id: Optional[int] = None) -> Optional[School]:
-        if school_id is not None:
-            return School.objects.get(pk=school_id)
-        else:
-            return None
+    def resolve_school(self, info: ResolveInfo, school_id: int) -> School:
+        return School.objects.get(pk=school_id)

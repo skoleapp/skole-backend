98b94c4605a7ad29336057fe7fcf8529fe5433bb
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-12-20 15:31:43 +0200

    Update subject api

--- src/api/schemas/subject.py
+++ src/api/schemas/subject.py
@@ -8,32 +8,17 @@ from app.models import Subject
 
 
 class SubjectType(DjangoObjectType):
-    school_count = graphene.Int()
-    course_count = graphene.Int()
-
     class Meta:
         model = Subject
         fields = ("id", "name")
 
-    def resolve_school_count(self, info: ResolveInfo) -> int:
-        return self.schools.count()
-
-    def resolve_course_count(self, info: ResolveInfo) -> int:
-        return self.courses.count()
-
 
 class Query(graphene.ObjectType):
-    subjects = graphene.List(SubjectType, school_id=graphene.String())
+    subjects = graphene.List(SubjectType)
     subject = graphene.Field(SubjectType, subject_id=graphene.Int())
 
-    def resolve_subjects(self, info: ResolveInfo, school_id: Optional[str] = None) -> List[Subject]:
-        if school_id is not None:
-            return Subject.objects.filter(schools__pk=school_id)
-        else:
-            return Subject.objects.all()
+    def resolve_subjects(self, info: ResolveInfo) -> List[Subject]:
+        return Subject.objects.all()
 
     def resolve_subject(self, info: ResolveInfo, subject_id: Optional[int] = None) -> Optional[Subject]:
-        if subject_id is not None:
-            return Subject.objects.get(pk=subject_id)
-        else:
-            return None
+        return Subject.objects.get(pk=subject_id)

e8ca2d3025fa56c65795f4b39f6f3d6b0a52e8a5
Markus Blomqvist <blomqvist_markus@hotmail.com>
2019-12-28 11:24:53 +0200

    Remove types folder from api

--- src/api/types/resource_part.py
+++
@@ -1,9 +0,0 @@
-from graphene_django import DjangoObjectType
-
-from app.models import ResourcePart
-
-
-class ResourcePartType(DjangoObjectType):
-    class Meta:
-        model = ResourcePart
-        fields = ("id", "resource")

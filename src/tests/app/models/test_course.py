8283d3da26be3c0371dc115803778d007f1782b0
ruohola <eero@ruohola.dev>
2020-01-21 17:08:29 +0200

    Add tests for Course model

--- src/tests/app/models/test_course.py
+++ src/tests/app/models/test_course.py
@@ -1,12 +1,16 @@
 from pytest import fixture
 
+from app.models import Course
 
-def test_str(course: fixture) -> None:
-    assert str(course) == "TEST0001 Test course"
 
-    course.code = None
-    assert str(course) == "Test course"
+def test_str(db: fixture) -> None:
+    course1 = Course.objects.get(pk=1)
+    assert str(course1) == "TEST0001 Test Engineering Course 1"
 
-    course.code = "TEST0001"
-    course.name = None
-    assert str(course) == "TEST0001"
+    course2 = Course.objects.get(pk=2)
+    course2.name = None
+    assert str(course2) == "TEST0002"
+
+    course3 = Course.objects.get(pk=3)
+    course3.code = None
+    assert str(course3) == "Test Engineering Course 3"

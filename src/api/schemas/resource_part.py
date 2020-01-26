3a4dbd17ead2aa7064b94bc2aac4e8cb98a1d4a9
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-01-26 10:15:53 +0200

    Resolve file in resource part API

--- src/api/schemas/resource_part.py
+++ src/api/schemas/resource_part.py
@@ -1,5 +1,5 @@
 from graphene_django import DjangoObjectType
-
+from graphql import ResolveInfo
 from app.models import ResourcePart
 
 
@@ -7,3 +7,6 @@ class ResourcePartObjectType(DjangoObjectType):
     class Meta:
         model = ResourcePart
         fields = ("id", "title", "file", "text", "file")
+
+    def resolve_file(self, info: ResolveInfo) -> str:
+        return f"media/{self.file}"

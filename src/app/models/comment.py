8c5431d11e257630f55be63878a67105d8aa2e06
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-02-02 23:08:58 +0200

    Add id as fallback comment ordering parameter

--- src/app/models/comment.py
+++ src/app/models/comment.py
@@ -46,7 +46,7 @@ class CommentManager(models.Manager):
     def get_queryset(self) -> QuerySet:
         qs = super().get_queryset()
         return qs.annotate(reply_count=models.Count("reply_comments")).order_by(
-            "-reply_count"
+            "-reply_count", "id"
         )
 
 

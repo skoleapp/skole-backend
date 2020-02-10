fd6e10a9b2fa0db3bbcbaa6b2ad1bbc5fed902f1
Eero Ruohola <eero@ruohola.dev>
2020-02-10 21:56:49 +0200

    Change fields of BetaCode to have unique=True

--- src/app/models/beta_code.py
+++ src/app/models/beta_code.py
@@ -4,5 +4,5 @@ from .user import User
 
 
 class BetaCode(models.Model):
-    code = models.CharField(max_length=6)
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    code = models.CharField(max_length=6, unique=True)
+    user = models.ForeignKey(User, unique=True, on_delete=models.SET_NULL, null=True)

a6e77c6f4fd6d6108967a4a820eaf0eab85f21e7
Eero Ruohola <eero@ruohola.dev>
2020-02-14 17:41:27 +0200

    Add tests for Subject

---
+++ src/tests/app/models/test_subject.py
@@ -0,0 +1,11 @@
+from pytest import fixture
+
+from app.models import Subject
+
+
+def test_str(db: fixture) -> None:
+    subject1 = Subject.objects.get(pk=1)
+    assert str(subject1) == "Computer Engineering"
+
+    subject2 = Subject.objects.get(pk=2)
+    assert str(subject2) == "Computer Science"

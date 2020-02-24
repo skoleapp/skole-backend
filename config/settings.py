ada519d6fd2313105a01119e32e4f6e416112cd9
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-02-24 21:57:45 +0200

    Update locales

--- config/settings.py
+++ config/settings.py
@@ -2,21 +2,17 @@ import datetime
 import os
 
 import dj_database_url  # type: ignore [import]
+from django.utils.translation import gettext_lazy as _
 
 DEBUG = os.environ.get("DEBUG")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DATABASES = {"default": dj_database_url.config()}
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", default="*").split()
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 ROOT_URLCONF = "config.urls"
-PASSWORD_MIN_LENGTH = 6
 WSGI_APPLICATION = "config.wsgi.application"
-LANGUAGE_CODE = "en-us"
-TIME_ZONE = "UTC"
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
 AUTH_USER_MODEL = "core.User"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+PASSWORD_MIN_LENGTH = 6
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", default="*").split()
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = ("http://localhost:3001",)
 AWS_S3_BUCKET_NAME_STATIC = os.environ.get("AWS_S3_BUCKET_NAME_STATIC")
@@ -24,9 +20,16 @@ MEDIA_URL = os.environ.get("MEDIA_URL", default="/media/")
 STATIC_URL = os.environ.get("STATIC_URL", default="/static/")
 MEDIA_ROOT = "media"
 STATIC_ROOT = "static"
+LANGUAGE_CODE = "en"
+TIME_ZONE = "UTC"
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
 
 if DEBUG is False:
     STATICFILES_STORAGE = "django_s3_storage.storage.StaticS3Storage"
+
+LANGUAGES = [("en", _("English")), ("fi", _("Finnish")), ("sv", _("Swedish"))]
 
 INSTALLED_APPS = [
     "django.contrib.admin",

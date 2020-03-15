568d60a92b08362d6084e7764723ebd1747c0d21
Eero Ruohola <eero@ruohola.dev>
2020-03-15 16:40:27 +0200

    Move decreasing of BetaCode's usages to a manager

--- core/models/beta_code.py
+++ core/models/beta_code.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+class BetaCodeManager(models.Manager):
+    def decrement_usages(self, code: "BetaCode") -> "BetaCode":
+        code.usages -= 1
+        code.save()
+        return code
+
+
 class BetaCode(models.Model):
     code = models.CharField(max_length=8, unique=True)
     usages = models.IntegerField()
+
+    objects = BetaCodeManager()

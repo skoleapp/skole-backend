19f134e5bdfcd25f18cc5acb6594f88d92267cbf
Eero Ruohola <eero@ruohola.dev>
2020-03-25 22:00:30 +0200

    Reverse changes in resource.py

--- api/forms/resource.py
+++ api/forms/resource.py
@@ -5,6 +5,8 @@ from core.models import Resource
 
 
 class CreateUpdateResourceForm(forms.ModelForm):
+    file = forms.CharField(required=False)
+
     class Meta:
         model = Resource
         fields = ("id", "title", "resource_type", "course", "date")

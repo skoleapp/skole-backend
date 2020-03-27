13675e22d2e0f1cb6e53ced4a24fd2c8283c4313
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-03-27 17:01:47 +0200

    Add pagination back

--- api/schemas/user.py
+++ api/schemas/user.py
@@ -1,4 +1,4 @@
-from typing import Any, List, Optional, Union
+from typing import Any, List, Literal, Optional, Union
 
 import graphene
 from django.contrib.auth import get_user_model
@@ -7,7 +7,7 @@ from django.utils.translation import gettext as _
 from graphene_django import DjangoObjectType
 from graphene_django.forms.mutation import DjangoModelFormMutation
 from graphene_django.types import ErrorType
-from graphql import ResolveInfo
+from graphql import GraphQLError, ResolveInfo
 from graphql_jwt.decorators import login_required, token_auth
 from mypy.types import JsonDict
 
@@ -18,6 +18,7 @@ from api.forms.user import (
     RegisterForm,
     UpdateUserForm,
 )
+from api.utils.pagination import PaginationMixin, get_paginator
 from core.models import BetaCode, User, Vote
 
 
@@ -68,7 +69,13 @@ class UserObjectType(DjangoObjectType):
         return self.avatar_thumbnail.url if self.avatar_thumbnail else ""
 
 
+class PaginatedUserObjectType(PaginationMixin, graphene.ObjectType):
+    objects = graphene.List(UserObjectType)
+
+
 class RegisterMutation(DjangoModelFormMutation):
+    user = graphene.Field(UserObjectType)
+
     class Meta:
         form_class = RegisterForm
         exclude_fields = ("id",)
@@ -85,11 +92,11 @@ class RegisterMutation(DjangoModelFormMutation):
 
         code = form.cleaned_data["code"]
         BetaCode.objects.decrement_usages(code)
-
         return cls(user=user)
 
 
 class LoginMutation(DjangoModelFormMutation):
+    user = graphene.Field(UserObjectType)
     token = graphene.String()
 
     class Meta:
@@ -128,6 +135,8 @@ class LoginMutation(DjangoModelFormMutation):
 
 
 class ChangePasswordMutation(DjangoModelFormMutation):
+    user = graphene.Field(UserObjectType)
+
     class Meta:
         form_class = ChangePasswordForm
         exclude_fields = ("id",)
@@ -170,6 +179,8 @@ class DeleteUserMutation(DjangoModelFormMutation):
 
 
 class UpdateUserMutation(DjangoModelFormMutation):
+    user = graphene.Field(UserObjectType)
+
     class Meta:
         form_class = UpdateUserForm
         exclude_fields = ("id",)
@@ -199,9 +210,14 @@ class UpdateUserMutation(DjangoModelFormMutation):
 
 
 class Query(graphene.ObjectType):
-    users = graphene.List(
-        UserObjectType, username=graphene.String(), ordering=graphene.String()
+    users = graphene.Field(
+        PaginatedUserObjectType,
+        page=graphene.Int(),
+        page_size=graphene.Int(),
+        username=graphene.String(),
+        ordering=graphene.String(),
     )
+
     user = graphene.Field(UserObjectType, id=graphene.ID())
     user_me = graphene.Field(UserObjectType)
 
@@ -209,18 +225,36 @@ class Query(graphene.ObjectType):
     def resolve_users(
         self,
         info: ResolveInfo,
+        page: int = 1,
+        page_size: int = 10,
         username: Optional[str] = None,
-        ordering: Optional[str] = None,
+        ordering: Optional[
+            Literal["username", "-username", "points", "-points"]
+        ] = None,
     ) -> Union["QuerySet[User]", List[User]]:
         qs = get_user_model().objects.filter(is_superuser=False)
 
         if username is not None:
-            qs = qs.filter(username=username)
+            qs = qs.filter(username__icontains=username)
 
-        if ordering in ["username", "-username", "points", "-points"]:
-            return qs.order_by(ordering)
+        if ordering is not None and ordering not in {
+            "username",
+            "-username",
+            "points",
+            "-points",
+        }:
+            raise GraphQLError("Invalid ordering value.")
 
-        return qs
+        if ordering in {"username", "-username"}:
+            qs = qs.order_by(ordering)
+        elif ordering == "points":
+            # Ignore: qs changes from QuerySet to a List, get_paginator handles that.
+            qs = sorted(qs, key=lambda u: u.points)  # type: ignore[assignment]
+        else:  # -points
+            # Ignore: Same as above.
+            qs = sorted(qs, key=lambda u: u.points, reverse=True)  # type: ignore[assignment]
+
+        return get_paginator(qs, page_size, page, PaginatedUserObjectType)
 
     @login_required
     def resolve_user(

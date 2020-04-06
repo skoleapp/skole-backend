47efd937b61a052ffeec0325d74121a4cd2d8dc1
Eero Ruohola <eero@ruohola.dev>
2020-04-07 00:24:07 +0300

    Monkey patch TranslatedFieldsModelMixin.__str__ for better admin experience

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.apps import apps
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from parler.models import TranslatedFieldsModelMixin
 
 core = apps.get_app_config("core")
 
@@ -9,3 +10,9 @@ for model_name, model in core.models.items():
         admin.site.register(model)
 
 admin.site.register(get_user_model())
+
+# We monkey patch the method here so that it's used for every single translated model.
+# Without this would just be displayed as "Finnish" or "English" in the object list.
+TranslatedFieldsModelMixin.__str__ = (
+    lambda self: f"{self.master} - {self.language_code}"
+)

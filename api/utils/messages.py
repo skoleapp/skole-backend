9d87d947a65952ec10ff9da3eb2b88cfb4a17374
Eero Ruohola <eero@ruohola.dev>
2020-04-07 23:09:39 +0300

    Add Swedish .po translations for strings

--- api/utils/messages.py
+++ api/utils/messages.py
@@ -1,4 +1,4 @@
 from django.utils.translation import gettext_lazy as _
 
-AUTH_ERROR_MESSAGE = _("Unable to authenticate with the provided credentials.")
+AUTH_ERROR_MESSAGE = _("Invalid username or password.")
 NOT_OWNER_MESSAGE = _("You are not the owner of this object.")

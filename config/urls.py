fcf2bff540b5cf17c158c63819d8bd4476d7a161
Eero Ruohola <eero@ruohola.dev>
2020-04-07 19:33:30 +0300

    Add language selection to admin, en/admin/, /fi/admin/

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
@@ -7,7 +8,8 @@ from django.views.decorators.csrf import csrf_exempt
 from api.views import CustomGraphQLView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    i18n_patterns(path("admin/", admin.site.urls)),
+    # FIXME: make frontend work without exempt.
     path(
         "graphql/",
         csrf_exempt(CustomGraphQLView.as_view(graphiql=bool(settings.DEBUG))),

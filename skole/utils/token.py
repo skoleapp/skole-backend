b4d2b315f327315ca098bfd799c932a613fcc755
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-04-26 18:50:44 +0300

    Remove type ignore from token utils

--- skole/utils/token.py
+++ skole/utils/token.py
@@ -1,14 +1,16 @@
 from datetime import timedelta
-from typing import Any, Union
+from typing import TYPE_CHECKING, Any, Union
 
 from django.core import signing
 from mypy.types import JsonDict
 
 from skole.utils.exceptions import TokenScopeError
 
+if TYPE_CHECKING:
+    from skole.models import User
 
-# Ignore: `User` model cannot be imported due to circular import or `AUTH_USER_MODEL` not being defined.
-def get_token(user: "User", action: str, **kwargs: Any) -> str:  # type: ignore [name-defined]
+
+def get_token(user: "User", action: str, **kwargs: Any) -> str:
     username = user.username
 
     if hasattr(username, "pk"):
@@ -23,8 +25,7 @@ def get_token(user: "User", action: str, **kwargs: Any) -> str:  # type: ignore 
     return token
 
 
-# Ignore: same as above.
-def revoke_user_refresh_tokens(user: "User") -> None:  # type: ignore [name-defined]
+def revoke_user_refresh_tokens(user: "User") -> None:
     refresh_tokens = user.refresh_tokens.all()
 
     for refresh_token in refresh_tokens:

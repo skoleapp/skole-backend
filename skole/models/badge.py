8173697905e23be86632a36e001b1bc2ecec226f
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-04-28 20:59:40 +0300

    Add badge model

---
+++ skole/models/badge.py
@@ -0,0 +1,17 @@
+from parler.models import TranslatableModel, TranslatedFields
+from django.db import models
+from django.conf import settings
+
+
+class Badge(TranslatableModel):
+    """Models a badge awarded for a user, e.g `Moderator`."""
+
+    user = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="badges", null=True)
+
+    translations = TranslatedFields(
+        name=models.CharField(max_length=100),
+        bio = models.TextField(max_length=2000, blank=True)
+    )
+
+    def __str__(self) -> str:
+        return f"{self.name}"

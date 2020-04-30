605a064d3e101b925764c2881eb3b760c18db5bd
Markus Blomqvist <blomqvist_markus@hotmail.com>
2020-04-30 13:18:01 +0300

    Remove name from contact form

--- skole/forms/contact.py
+++ skole/forms/contact.py
@@ -3,6 +3,5 @@ from django import forms
 
 class ContactForm(forms.Form):
     subject = forms.CharField(max_length=25)
-    name = forms.CharField(max_length=50, required=False)
     email = forms.EmailField()
     message = forms.CharField(max_length=2000)

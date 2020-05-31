fac5a0b8a85081b4af8074c0c2276231bb170ed5
Eero Ruohola <eero@ruohola.dev>
2020-05-31 17:43:16 +0300

    Add tests for health check view

---
+++ skole/tests/views/test_health_check.py
@@ -0,0 +1,8 @@
+from django.test.client import Client
+
+
+def test_health_check() -> None:
+    client = Client()
+    response = client.get("/healthz/")
+    assert response.status_code == 200
+    assert response.content == b""

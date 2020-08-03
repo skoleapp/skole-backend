cda069824a41864be7878851cd30cd9fe79e137e
Eero Ruohola <eero@ruohola.dev>
2020-08-03 18:39:25 +0300

    Make / redirect to admin for convenience
    No real reason to have the index url return 404.

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,9 @@ from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import RedirectView
 from graphql_jwt.decorators import jwt_cookie
 
 from skole.views import SkoleGraphQLView, health_check
@@ -18,6 +19,7 @@ urlpatterns = [
         ),
     ),
     path("healthz/", health_check),
+    path("", RedirectView.as_view(url=reverse_lazy("admin:index"))),
 ]
 
 urlpatterns += i18n_patterns(path("admin/", admin.site.urls))

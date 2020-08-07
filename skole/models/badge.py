205312431cc89d38f92315e514c1ecc5675f75b3
Eero Ruohola <eero@ruohola.dev>
2020-08-07 03:02:36 +0300

    Use correct subclass for Badge model
    Was getting an error in QA admin.

--- skole/models/badge.py
+++ skole/models/badge.py
@@ -1,11 +1,11 @@
 from django.conf import settings
 from django.db import models
-from parler.models import TranslatableModel, TranslatedFields
+from parler.models import TranslatedFields
 
-from .base import SkoleModel
+from skole.models.base import TranslatableSkoleModel
 
 
-class Badge(SkoleModel, TranslatableModel):
+class Badge(TranslatableSkoleModel):
     """Models a badge awarded for a user, e.g `Moderator`."""
 
     user = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="badges")

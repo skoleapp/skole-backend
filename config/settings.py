73b600203c86fa7e0fd9fc9c67bebf345c6234a5
Eero Ruohola <eero@ruohola.dev>
2020-11-15 23:41:12 +0200

    Move CORS middleware higher up in the list
    The docs suggest putting it at least before `CommonMiddleware`:
    https://github.com/adamchainz/django-cors-headers#setup

--- config/settings.py
+++ config/settings.py
@@ -69,12 +69,12 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "skole.middleware.SkoleSessionMiddleware",
     "django.middleware.locale.LocaleMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 # Password validator settings

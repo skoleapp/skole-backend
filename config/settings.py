7ad21d3fb447c5b8cfb5944c701d1ee404ecca18
Markus Blomqvist <markus@skoleapp.com>
2021-01-18 12:49:18 +0200

    Document the suggestion count setttings

--- config/settings.py
+++ config/settings.py
@@ -229,6 +229,8 @@ USER_AVATAR_ALLOWED_FILETYPES = [("image/jpeg", "JPEG"), ("image/png", "PNG")]
 # Shorter rate limiting in dev env for increased convenience.
 MY_DATA_RATE_LIMIT = timedelta(seconds=5) if DEBUG else timedelta(minutes=10)
 
+# Both values below are divisible by three so we can include the same amount of courses, resources and comments in the suggestions.
+
 # Amount of results returned by the suggestions query.
 SUGGESTIONS_COUNT = 21
 

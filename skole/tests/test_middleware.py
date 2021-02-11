4cd86dc56347ac244562e6bffb376677fed05d82
Eero Ruohola <eero@skoleapp.com>
2021-02-11 12:14:55 +0200

    Fix middleware tests not resetting global state properly
    Previously the `graphene_django.views` module would not get reloaded if
    the test case failed.

--- skole/tests/test_middleware.py
+++ skole/tests/test_middleware.py
@@ -1,10 +1,8 @@
-import importlib
-
 import graphene_django.views
 from django.conf import settings
 from django.test import override_settings
 
-from skole.tests.helpers import SkoleSchemaTestCase
+from skole.tests.helpers import SkoleSchemaTestCase, reload_module
 
 
 class MiddlewareTests(SkoleSchemaTestCase):
@@ -30,39 +28,37 @@ class MiddlewareTests(SkoleSchemaTestCase):
     """
 
     def test_introspection_enabled(self) -> None:
-        with override_settings(GRAPHENE=settings.GRAPHENE | {"MIDDLEWARE": []}):
+        # Calling `override_settings` will trigger
+        # `graphene_django.settings.reload_graphene_settings` just fine.
+        # One could think that that's enough to get the new middleware config
+        # into use, but because `graphene_django.views` has already imported
+        # `graphene_settings`, and `reload_graphene_settings` simply reassigns the
+        # name, the object in the `views` module will keep pointing to the original
+        # settings instance. Thus we have to manually reload the whole module,
+        # to make sure that the `graphene_settings` name in the `views` module
+        # will point to the refreshed object. Only that way when our view class is
+        # initialized its `middleware` attribute will correctly get the new value.
+        #
+        # The context manager also reloads the module again afterwards so that the
+        # next tests will just use the middleware settings from `settings.py`.
+        with reload_module(graphene_django.views) as loader:
+            with override_settings(GRAPHENE=settings.GRAPHENE | {"MIDDLEWARE": []}):
+                loader()
+                res = self.execute(self.schema_query)
+                assert isinstance(res["types"][0]["name"], str)
 
-            # Calling `override_settings` will trigger
-            # `graphene_django.settings.reload_graphene_settings` just fine.
-            # One could think that that's enough to get the new middleware config
-            # into use, but because `graphene_django.views` has already imported
-            # `graphene_settings`, and `reload_graphene_settings` simply reassigns the
-            # name, the object in the `views` module will keep pointing to the original
-            # settings instance. Thus we have to manually reload the whole module,
-            # to make sure that the `graphene_settings` name in the `views` module
-            # will point to the refreshed object. Only that way when our view class is
-            # initialized its `middleware` attribute will correctly get the new value.
-            importlib.reload(graphene_django.views)
-
-            res = self.execute(self.schema_query)
-            assert isinstance(res["types"][0]["name"], str)
-
-            res = self.execute(self.normal_query, variables={"id": 1})
-            assert res["id"] == "1"
-
-        # So that next tests will just use the middleware settings from `settings.py`.
-        importlib.reload(graphene_django.views)
+                res = self.execute(self.normal_query, variables={"id": 1})
+                assert res["id"] == "1"
 
     def test_introspection_disabled(self) -> None:
-        with override_settings(
-            GRAPHENE=settings.GRAPHENE
-            | {"MIDDLEWARE": ["skole.middleware.DisableIntrospectionMiddleware"]}
-        ):
-            importlib.reload(graphene_django.views)
+        with reload_module(graphene_django.views) as loader:
+            with override_settings(
+                GRAPHENE=settings.GRAPHENE
+                | {"MIDDLEWARE": ["skole.middleware.DisableIntrospectionMiddleware"]}
+            ):
+                loader()
+                self.execute(self.schema_query, assert_error=True)
 
-            self.execute(self.schema_query, assert_error=True)
-
-            res = self.execute(self.normal_query, variables={"id": 1})  # No error here.
-            assert res["id"] == "1"
-
-        importlib.reload(graphene_django.views)
+                # Fine to make a query which doesn't introspect the schema.
+                res = self.execute(self.normal_query, variables={"id": 1})
+                assert res["id"] == "1"

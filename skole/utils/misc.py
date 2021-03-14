1ed2be417a1c1cfbcd5f290ba2d010e4587ab44d
Eero Ruohola <eero@skoleapp.com>
2021-03-14 14:50:35 +0200

    Fix migration errors with existing non-unique course slugs
    Also, make `slugify` truncate the slug first, so that it can remove all
    trailing special characters.

--- skole/utils/misc.py
+++ skole/utils/misc.py
@@ -2,30 +2,29 @@ import re
 
 import autoslug.utils
 
-NON_ALPHANUM = re.compile(r"[^a-zA-Z0-9]")
+TRAILING_NON_ALPHANUM = re.compile(r"[^a-zA-Z0-9]+$")
 
 
 def slugify(value: str) -> str:
     """
     Improves upon `autoslug.utils.slugify` by preventing trailing special characters.
 
-    In practice this happens so that this makes sure that the char at the 50th index
-    is not a special character. If it is, this replaces it with the letter 'a'.
+    This also pre-truncates the slug to 50 characters, which is the default for the
+    `AutoSlugField.max_length`, and the value that we're always using. This would be
+    done in the AutoSlugField after slugifying, but we need to do it here to make
+    sure that the final character is alphanumeric.
 
     Used as the `AUTOSLUG_SLUGIFY_FUNCTION` in `settings.py`.
 
     Examples:
-        >>> slugify("b" * 49 + "-b")
-        'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbab'
+        >>> slugify("b" * 49 + "-aaa")
+        'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
+        >>> slugify("b" * 49 + "--")
+        'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
         >>> slugify(50 * "b")
         'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb'
         >>> slugify("_____")
-        '_____'
+        ''
     """
-    slug = autoslug.utils.slugify(value)
-    try:
-        if re.match(NON_ALPHANUM, slug[49]):
-            slug = slug[:49] + "a" + slug[50:]
-    except IndexError:
-        pass
-    return slug
+    slug = autoslug.utils.slugify(value)[:50]
+    return re.sub(TRAILING_NON_ALPHANUM, "", slug)
